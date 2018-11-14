a56e5a62cd23f60d62336e529d5769a5dc28972f
Rachel Klesius <rachelklesius@gmail.com>
2018-11-14 00:11:43 -0500

    Semaphores done, processes calling correctly

--- thanksgiving.c
+++ thanksgiving.c
@@ -4,6 +4,7 @@
 //By Rachel Klesius and Garrett Moncrief
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -25,6 +26,7 @@ typedef struct
 {
   int value;   // Count of how many passes
   int curPos;  // Current location of process
+  int need;    // How many people need this dish
 } shared_mem;
 
 // Food shared resources, values of these will indicate how many times they have
@@ -159,19 +161,31 @@ int main()
 		exit(0);
 	}
 
-  //initialize the pass count to zero and location to -1, showing that no one has picked it up yet
+  //initialize the pass count to zero and location to -1, showing no one has picked it up
+  //Also, set the need of each resources based on what people want
 	turkey->value = 0;
   turkey->curPos = -1; 
+  turkey->need = 4;
+  
   gravy->value = 0;
   gravy->curPos = -1;
+  gravy->need = 4;
+  
   mash->value = 0;
   mash->curPos = -1;
+  mash->need = 3;
+  
   stuffing->value = 0;
   stuffing->curPos = -1;
+  stuffing->need = 2;
+  
   peas->value = 0;
   peas->curPos = -1;
+  peas->need = 3;
+  
   cranberry->value = 0;
   cranberry->curPos = -1;
+  cranberry->need = 2;
 	
 	// Create Semaphores
 	sem_id = semget(SEMKEY, NSEMS, IPC_CREAT | 0666);
@@ -192,7 +206,7 @@ int main()
  
 	if ((pid1 = fork()) == 0)
 	{
-		rachel(1);
+		rachel(3);
 	}
 	if ((pid3 = fork()) == 0)
 	{
@@ -200,18 +214,18 @@ int main()
 	}
 	if ((pid4 = fork()) == 0)
 	{
-		megan(3);
+		megan(1);
 	}
 	if ((pid2 = fork()) == 0)
 	{
-		garrett(4);
+		garrett(0);
 	}
 
 	// Wait for child processes and display id
+  int ID4 = wait(&pid4);
 	int ID1 = wait(&pid1);
 	int ID2 = wait(&pid2);
 	int ID3 = wait(&pid3);
-	int ID4 = wait(&pid4);
 	printf("Child with ID: %d has just exited\n", ID1);
 	printf("Child with ID: %d has just exited\n", ID2);
 	printf("Child with ID: %d has just exited\n", ID3);
@@ -255,165 +269,526 @@ int main()
 void rachel(int position)
 {	
   // Calculate neighbors for this position
-  int leftNeighbor;
-  int rightNeighbor;
+  int neighbor;
+  
+  //To make sure Rachel gets her grub
+  bool gotTurkey = false;
+  bool gotGravy = false;
+  bool gotMash = false;
+  bool gotPeas = false;
+  bool gotCranberry = false;
+  
+  //what Rachel will pass if she needs to
+  bool passedStuffing = false;
   
   if(position == 0)  //first in row
   {
-    leftNeighbor = PEOPLE_COUNT - 1;
-    rightNeighbor = 1;
-  
+    neighbor = PEOPLE_COUNT - 1;
+  }
   else if(position == PEOPLE_COUNT - 1)  //last in row
   {
-    leftNeighbor = PEOPLE_COUNT - 2;
-    rightNeighbor = 0;
+    neighbor = PEOPLE_COUNT - 2;
   }
   
   else 
   {
-    leftNeighbor = position - 1;
-    rightNeighbor = position + 1;
+    neighbor = position - 1;
   }
   
-  while(true) 
+  while(!gotTurkey) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor has it, take it
+    if (turkey->curPos == -1 || turkey->curPos == neighbor)
+    {
+      turkey->value++;
+      turkey->need--;
+      turkey->curPos = position;
+      gotTurkey = true;
+    }
+    VOP();
+  }
+  printf("\nRachel got turkey, turkey passed: %d times\n", turkey->value);
+  shmdt(turkey);
+  
+  while(!gotGravy) 
   {
     POP(); 
     //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
-    if (turkey->curPos == -1 || turkey->curPos == leftNeighbor || turkey->curPos == rightNeighbor)  //TODO: change to check for neighbors
+    if (gravy->curPos == -1 || gravy->curPos == neighbor)
     {
-      turkey->value++;
-      break;
+      gravy->value++;
+      gravy->need--;
+      gravy->curPos = position;
+      gotGravy = true;
     }
-    turkey->curPos = position;
     VOP();
   }
-  printf("\nTurkey passed: %d times\n", turkey->value);
-  shmdt(turkey);	
- 
- /*
-  POP(); 
-  gravy->value++;
-  VOP();
-  printf("Gravy passed: %d times\n", gravy->value);
+  printf("Rachel got gravy, gravy passed: %d times\n", gravy->value);
   shmdt(gravy);
   
-  POP(); 
-  mash->value++;
-  VOP();
-  printf("Mashed Potatoes passed: %d times\n", mash->value);
-  shmdt(mash);
+  while(!gotMash) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (mash->curPos == -1 || mash->curPos == neighbor)
+    {
+      mash->value++;
+      mash->need--;
+      mash->curPos = position;
+      gotMash = true;
+    }
+    VOP();
+  }
+  printf("Rachel got mashed potatoes, mashed potatoes passed: %d times\n", mash->value);
+  shmdt(mash);	
   
-  POP(); 
-  peas->value++;
-  VOP();
-  printf("Peas passed: %d times\n", peas->value);
+  //Rachel doesn't want stuffing, but she still has to pass it if she knows other people need it and it comes to her
+  while(!passedStuffing)
+  {
+    POP();
+    //if her neighbor gets stuffing and there is still a need for stuffing she must pass it on
+    if((stuffing->need > 0) && (stuffing->curPos == neighbor))
+    {
+      stuffing->value++;
+      stuffing->curPos = position;
+      passedStuffing = true;
+    }
+    
+    //if there is no more need for stuffing, exit this loop without adding to passing count
+    if(stuffing->need == 0)
+    {
+      passedStuffing = true;
+    }
+    VOP();
+  }
+  shmdt(stuffing);
+ 
+  while(!gotPeas) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (peas->curPos == -1 || peas->curPos == neighbor)
+    {
+      peas->value++;
+      peas->need--;
+      peas->curPos = position;
+      gotPeas = true;
+    }
+    VOP();
+  }
+  printf("Rachel got peas, peas passed: %d times\n", peas->value);
   shmdt(peas);
   
-  POP(); 
-  cranberry->value++;
-  VOP();
-  printf("Cranberry sauce passed: %d times\n", cranberry->value);
+  while(!gotCranberry) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (cranberry->curPos == -1 || cranberry->curPos == neighbor)
+    {
+      cranberry->value++;
+      cranberry->need--;
+      cranberry->curPos = position;
+      gotCranberry = true;
+    }
+    VOP();
+  }
+  printf("Rachel got cranberry sauce, cranberry sauce passed: %d times\n", cranberry->value);
   shmdt(cranberry);
-  */
  
 	_exit(0);
 }
 
 //Garrett wants everything from thanksgiving
-void garrett()
+void garrett(int position)
 {
-	POP(); 
-  turkey->value++;
-  VOP();
-  printf("\nTurkey passed: %d times\n", turkey->value);
+  // Calculate neighbor for this position
+  int neighbor;
+
+  bool gotTurkey = false;
+  bool gotGravy = false;
+  bool gotMash = false;
+  bool gotStuffing = false;
+  bool gotPeas = false;
+  bool gotCranberry = false;
+  
+  if(position == 0)  //first in row
+  {
+    neighbor = PEOPLE_COUNT - 1;
+  }
+  else if(position == PEOPLE_COUNT - 1)  //last in row
+  {
+    neighbor = PEOPLE_COUNT - 2;
+  }
+  
+  else 
+  {
+    neighbor = position - 1;
+  }
+  
+  while(!gotTurkey) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor has it, take it
+    if (turkey->curPos == -1 || turkey->curPos == neighbor)
+    {
+      turkey->value++;
+      turkey->need--;
+      turkey->curPos = position;
+      gotTurkey = true;
+    }	
+   VOP();
+  }
+  printf("Garrett got turkey, turkey passed: %d times\n", turkey->value);
   shmdt(turkey);
   
-  /*
-  POP(); 
-  gravy->value++;
-  VOP();
-  printf("Gravy passed: %d times\n", gravy->value);
+  while(!gotGravy) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (gravy->curPos == -1 || gravy->curPos == neighbor)
+    {
+      gravy->value++;
+      gravy->need--;
+      gravy->curPos = position;
+      gotGravy = true;
+    }
+    VOP();
+  }
+  printf("Garrett got gravy, gravy passed: %d times\n", gravy->value);
   shmdt(gravy);
   
-  POP(); 
-  mash->value++;
-  VOP();
-  printf("Mashed potatoes passed: %d times\n", mash->value);
+  while(!gotMash) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (mash->curPos == -1 || mash->curPos == neighbor)
+    {
+      mash->value++;
+      mash->need--;
+      mash->curPos = position;
+      gotMash = true;
+    }
+    VOP();
+  }
+  printf("Garrett got mashed potatoes, mashed potatoes passed: %d times\n", mash->value);
   shmdt(mash);
   
-  POP();
-  stuffing->value++;
-  VOP();
-  printf("Stuffing passed: %d times\n", stuffing->value);
+  while(!gotStuffing) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (stuffing->curPos == -1 || stuffing->curPos == neighbor)
+    {
+      stuffing->value++;
+      stuffing->need--;
+      stuffing->curPos = position;
+      gotStuffing = true;
+    }
+    VOP();
+  }
+  printf("Garrett got stuffing, stuffing passed: %d times\n", stuffing->value);
   shmdt(stuffing);
   
-  POP(); 
-  peas->value++;
-  VOP();
-  printf("Peas passed: %d times\n", peas->value);
+  while(!gotPeas) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (peas->curPos == -1 || peas->curPos == neighbor)
+    {
+      peas->value++;
+      peas->need--;
+      peas->curPos = position;
+      gotPeas = true;
+    }
+    VOP();
+  }
+  printf("Garrett got peas, peas passed: %d times\n", peas->value);
   shmdt(peas);
   
-  POP(); 
-  cranberry->value++;
-  VOP();
-  printf("\nCranberry sauce passed: %d times\n", cranberry->value);
+  while(!gotCranberry) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (cranberry->curPos == -1 || cranberry->curPos == neighbor)
+    {
+      cranberry->value++;
+      cranberry->need--;
+      cranberry->curPos = position;
+      gotCranberry = true;
+    }
+    VOP();
+  }
+  printf("Garrett got cranberry sauce, cranberry sauce passed: %d times\n", cranberry->value);
   shmdt(cranberry);
-  */
- 
+   
 	_exit(0);
 }
 
 //eddie wants turkey, gravy, and mashed potatoes
-void eddie()
+void eddie(int position)
 {	
-	POP(); 
-  turkey->value++;
-  VOP();
-  printf("\nTurkey passed: %d times\n", turkey->value);
-  shmdt(turkey);
-	/*
-  POP(); 
-  gravy->value++;
-  VOP();
-  printf("Gravy passed: %d times\n", gravy->value);
+	  // Calculate neighbors for this position
+  int neighbor;
+  bool gotTurkey = false;
+  bool gotGravy = false;
+  bool gotMash = false;
+  
+  bool passedStuffing = false;
+  bool passedPeas = false;
+  bool passedCranberry = false;
+  
+  if(position == 0)  //first in row
+  {
+    neighbor = PEOPLE_COUNT - 1;
+  }
+  else if(position == PEOPLE_COUNT - 1)  //last in row
+  {
+    neighbor = PEOPLE_COUNT - 2;
+  }
+  
+  else 
+  {
+    neighbor = position - 1;
+  }
+  
+  while(!gotTurkey) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor has it, take it
+    if (turkey->curPos == -1 || turkey->curPos == neighbor)
+    {
+      turkey->value++;
+      turkey->need--;
+      turkey->curPos = position;
+      gotTurkey = true;
+    }
+    VOP();
+  }
+  printf("\nEddie got turkey, turkey passed: %d times\n", turkey->value);
+  shmdt(turkey);	
+  
+  while(!gotGravy) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (gravy->curPos == -1 || gravy->curPos == neighbor)
+    {
+      gravy->value++;
+      gravy->need--;
+      gravy->curPos = position;
+      gotGravy = true;
+    }
+    VOP();
+  }
+  printf("Eddie got gravy, gravy passed: %d times\n", gravy->value);
   shmdt(gravy);
   
-  POP(); 
-  mash->value++;
-  VOP();
-  printf("\nTurkey passed: %d times\n", turkey->value);	
+  while(!gotMash) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (mash->curPos == -1 || mash->curPos == neighbor)
+    {
+      mash->value++;
+      mash->need--;
+      mash->curPos = position;
+      gotMash = true;
+    }
+    VOP();
+  }
+  printf("Eddie got mashed potatoes, mashed potatoes passed: %d times\n", mash->value);
   shmdt(mash);
- */
+  
+  while(!passedStuffing)
+  {
+    POP();
+    
+    if((stuffing->need > 0) && (stuffing->curPos == neighbor))
+    {
+      stuffing->value++;
+      stuffing->curPos = position;
+      passedStuffing = true;
+    }
+    
+    if(stuffing->need == 0)
+    {
+      passedStuffing = true;
+    }
+    VOP();
+  }
+  shmdt(stuffing);
+  
+  while(!passedPeas)
+  {
+    POP();
+    
+    if((peas->need > 0) && (peas->curPos == neighbor))
+    {
+      peas->value++;
+      peas->curPos = position;
+      passedPeas = true;
+    }
+    
+    if(peas->need == 0)
+    {
+      passedPeas = true;
+    }
+    VOP();
+  }
+  shmdt(peas);
+  
+  while(!passedCranberry)
+  {
+    POP();
+    
+    if((cranberry->need > 0) && (cranberry->curPos == neighbor))
+    {
+      cranberry->value++;
+      cranberry->curPos = position;
+      passedCranberry = true;
+    }
+    
+    if(cranberry->need == 0)
+    {
+      passedCranberry = true;
+    }
+    VOP();
+  }
+  shmdt(cranberry);
+  
 	_exit(0);
 }
 
 //megan wants turkey, gravy, stuffing, and peas
-void megan()
+void megan(int position)
 {	
-  POP(); 
-  turkey->value++;
-  VOP();
-  printf("\nTurkey passed: %d times\n", turkey->value);
-  shmdt(turkey);
-	/*
-  POP(); 
-  gravy->value++;
-  VOP();
-  printf("Gravy passed: %d times\n", gravy->value);
-  shmdt(gravy);
+  // Calculate neighbors for this position
+  int neighbor;
   
-  POP();
-  stuffing->value++;
-  VOP();
-  printf("Stuffing passed %d times\n", stuffing->value);
+  bool gotTurkey = false;
+  bool gotGravy = false;
+  bool gotStuffing = false;
+  bool gotPeas = false;
+  
+  bool passedMash = false;
+  bool passedCranberry = false;
+  
+  if(position == 0)  //first in row
+  {
+    neighbor = PEOPLE_COUNT - 1;
+  }
+  else if(position == PEOPLE_COUNT - 1)  //last in row
+  {
+    neighbor = PEOPLE_COUNT - 2;
+  }
+  
+  else 
+  {
+    neighbor = position - 1;
+  }
+  
+  while(!gotTurkey) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor has it, take it
+    if (turkey->curPos == -1 || turkey->curPos == neighbor)
+    {
+      turkey->value++;
+      turkey->need--;
+      turkey->curPos = position;
+      gotTurkey = true;
+    }
+    VOP();
+  }
+  printf("\nMegan got turkey, turkey passed: %d times\n", turkey->value);
+  shmdt(turkey);
+  
+  while(!gotGravy) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (gravy->curPos == -1 || gravy->curPos == neighbor)
+    {
+      gravy->value++;
+      gravy->need--;
+      gravy->curPos = position;
+      gotGravy = true;
+    }
+    VOP();
+  }
+  printf("Megan got gravy, gravy passed: %d times\n", gravy->value);
+  shmdt(gravy);	
+  
+  while(!passedMash)
+  {
+    POP();
+    
+    if((mash->need > 0) && (mash->curPos == neighbor))
+    {
+      mash->value++;
+      mash->curPos = position;
+      passedMash = true;
+    }
+    
+    if(mash->need == 0)
+    {
+      passedMash = true;
+    }
+    VOP();
+  }
+  shmdt(mash);
+  
+  while(!gotStuffing) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (stuffing->curPos == -1 || stuffing->curPos == neighbor)
+    {
+      stuffing->value++;
+      stuffing->need--;
+      stuffing->curPos = position;
+      gotStuffing = true;
+    }
+    VOP();
+  }
+  printf("Megan got stuffing, stuffing passed: %d times\n", stuffing->value);
   shmdt(stuffing);
   
-  POP(); 
-  peas->value++;
-  VOP();
-  printf("Peas passed %d times\n", peas->value);
+  while(!gotPeas) 
+  {
+    POP(); 
+    //if no one has picked it up yet or your neighbor last used the resource, then you get the resource
+    if (peas->curPos == -1 || peas->curPos == neighbor)
+    {
+      peas->value++;
+      peas->need--;
+      peas->curPos = position;
+      gotPeas = true;
+    }
+    VOP();
+  }
+  printf("Megan got peas, peas passed: %d times\n", peas->value);
   shmdt(peas);
-  */
+  
+  while(!passedCranberry)
+  {
+    POP();
+    
+    if((cranberry->need > 0) && (cranberry->curPos == neighbor))
+    {
+      cranberry->value++;
+      cranberry->curPos = position;
+      passedCranberry = true;
+    }
+    
+    if(cranberry->need == 0)
+    {
+      passedCranberry = true;
+    }
+    VOP();
+  }
+  shmdt(cranberry);
  
 	_exit(0);
 }
